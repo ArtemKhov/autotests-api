df8c9c593e05e521e0a253d25ebbcfc9b0c2ee2c
Artem <artem.kotyakhov@gmail.com>
2026-07-16 14:15:53 +0300

    add cURL to Allure reports

---
+++ clients/event_hooks.py
@@ -0,0 +1,20 @@
+import allure
+from httpx import Request
+
+from tools.http.curl import make_curl_from_request
+
+
+def curl_event_hook(request: Request):
+    """
+    Event hook для автоматического прикрепления cURL команды к Allure отчету.
+
+    :param request: HTTP-запрос, переданный в `httpx` клиент.
+    """
+    # Генерируем команду cURL из объекта запроса
+    curl_command = make_curl_from_request(request)
+
+    # Прикрепляем сгенерированную cURL команду к отчету Allure
+    # allure.attach() добавляет текстовый файл в отчет.
+    # "cURL command" — название вложения.
+    # allure.attachment_type.TEXT — указываем, что вложение текстовое.
+    allure.attach(curl_command, "cURL command", allure.attachment_type.TEXT)
