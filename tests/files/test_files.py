1b135c06f1c5227bdb90aacc7328c3e671c9c052
Artem <artem.kotyakhov@gmail.com>
2026-07-18 14:12:38 +0300

    add env and settings

--- tests/files/test_files.py
+++ tests/files/test_files.py
@@ -7,6 +7,7 @@ from allure_commons.types import Severity
 from clients.errors_schema import ValidationErrorResponseSchema, InternalErrorResponseSchema
 from clients.files.files_client import FilesClient
 from clients.files.files_schema import CreateFileRequestSchema, CreateFileResponseSchema, GetFileResponseSchema
+from config import settings
 from fixtures.files import FileFixture
 from tools.allure.epics import AllureEpic
 from tools.allure.features import AllureFeature
@@ -35,7 +36,7 @@ class TestFiles:
     @allure.title("Create file")
     def test_create_file(self, files_client: FilesClient):
         # файл, который будет отправляться на сервер
-        request = CreateFileRequestSchema(upload_file="./testdata/files/image.png")
+        request = CreateFileRequestSchema(upload_file=settings.test_data.image_png_file)
         response = files_client.create_file_api(request)
         response_data = CreateFileResponseSchema.model_validate_json(response.text)
 
@@ -87,8 +88,8 @@ class TestFiles:
     @allure.title("Create file with empty filename")
     def test_create_file_with_empty_filename(self, files_client: FilesClient):
         request = CreateFileRequestSchema(
-            filename="", # задаем пустое значения у имени файла
-            upload_file="./testdata/files/image.png"
+            filename="",
+            upload_file=settings.test_data.image_png_file
         )
         response = files_client.create_file_api(request)
         response_data = ValidationErrorResponseSchema.model_validate_json(response.text)
@@ -108,8 +109,8 @@ class TestFiles:
     @allure.title("Create file with empty directory")
     def test_create_file_with_empty_directory(self, files_client: FilesClient):
         request = CreateFileRequestSchema(
-            directory="", # задаем пустое значения у директории
-            upload_file="./testdata/files/image.png"
+            directory="",
+            upload_file=settings.test_data.image_png_file
         )
         response = files_client.create_file_api(request)
         response_data = ValidationErrorResponseSchema.model_validate_json(response.text)
